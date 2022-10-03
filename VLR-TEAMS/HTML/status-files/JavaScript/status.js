952987639237e94aa635dd290120bfc522e0605f
pedropegu <pgpedro0018@gmail.com>
2022-10-03 12:05:29 +0200

    HTML

--- VLR-TEAMS/HTML/status-files/JavaScript/status.js
+++ VLR-TEAMS/HTML/status-files/JavaScript/status.js
@@ -1,10 +1,19 @@
 
- var img = new Image();
- var div = document.getElementById('status');
-let response = fetch('https://api.henrikdev.xyz/valorant/v1/status/eu')
+let euw = fetch('https://api.henrikdev.xyz/valorant/v1/status/eu')
     .then((response) => {
-    div.innerHTML = "EUW: "
+    if (response["status"] == 200) {
+      document.getElementById('status-euw').innerHTML = "EU: No recent issues or events to report  " + '<i class="bi bi-check-circle-fill"></i>' 
+    } else {
+      document.getElementById('status-euw').innerHTML = "EUW: " + response["data"]["incidents"] + '  <i class="bi bi-x"></i>'
+    }
   })
-image = div.appendChild(img)
-img.src ='../../favicon.png'
+
+  let na = fetch('https://api.henrikdev.xyz/valorant/v1/status/na')
+  .then((response) => {
+  if (response["status"] == 200) {
+    document.getElementById('status-na').innerHTML = "NA: No recent issues or events to report  " + '<i class="bi bi-check-circle-fill"></i>' 
+  } else {
+    document.getElementById('status-na').innerHTML = "NA: " + response["data"]["incidents"] + '  <i class="bi bi-x"></i>'
+  }
+})
 
