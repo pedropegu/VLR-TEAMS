11bce1711162af773769aeb3ec3c0a556f134c2d
Ubuntu <ubuntu@ip-172-31-81-140.ec2.internal>
2022-11-16 20:39:06 +0000

    NGINX

--- VLR-TEAMS/VLR_TEAMS/settings.py
+++ VLR-TEAMS/VLR_TEAMS/settings.py
@@ -129,15 +129,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'src/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "src",
+    BASE_DIR / "static"
 ]
+
 STATIC_ROOT = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
